eb447c341ee1d8e170d6d92b44dd2a902a6df523
Louis <trinewbee@qq.com>
2019-11-13 20:47:25 +0800

    Nano.Logs.Log改名为MiniLog

--- nano/Nano.Common/Log/Log.cs
+++ nano/Nano.Common/Log/Log.cs
@@ -8,67 +8,67 @@ using Nano.Collection;
 
 namespace Nano.Logs
 {
-//    public class LogBuffer : RingBuffer<LogBuffer.Item>
-//    {
-//        public class Item
-//        {
-//            public long Ticks;  // Ticks of DateTime.UtcNow
-//            public string Line;
-//        }
+    public class MiniLogBuffer : RingBuffer<MiniLogBuffer.Item>
+    {
+        public class Item
+        {
+            public long Ticks;  // Ticks of DateTime.UtcNow
+            public string Line;
+        }
 
-//        public LogBuffer(int capacity) : base(capacity) { }
+        public MiniLogBuffer(int capacity) : base(capacity) { }
 
-//        public void Add(long ticks, string line)
-//        {
-//            var item = new Item { Ticks = ticks, Line = line };
-//            Add(item);
-//        }
-//    }
+        public void Add(long ticks, string line)
+        {
+            var item = new Item { Ticks = ticks, Line = line };
+            Add(item);
+        }
+    }
 
-//    public class Log
-//    {
-//        public LogBuffer Buffer { get; private set; }
+    public class MiniLog
+    {
+        public MiniLogBuffer Buffer { get; private set; }
 
-//        bool m_console;
-//        TextWriter m_tw;
+        bool m_console;
+        TextWriter m_tw;
 
-//        public Log(bool console = true, string path = null)
-//        {
-//            Buffer = new LogBuffer(1000);
-//            m_console = console;
+        public MiniLog(bool console = true, string path = null)
+        {
+            Buffer = new MiniLogBuffer(1000);
+            m_console = console;
 
-//            if (path != null)
-//            {
-//                var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
-//                fs.Seek(0, SeekOrigin.End);
-//                m_tw = new StreamWriter(fs, Encoding.UTF8);
-//            }
-//        }
+            if (path != null)
+            {
+                var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+                fs.Seek(0, SeekOrigin.End);
+                m_tw = new StreamWriter(fs, Encoding.UTF8);
+            }
+        }
 
-//        public void WriteLine(string s)
-//        {
-//            var dt = DateTime.UtcNow;
-//            Buffer.Add(dt.Ticks, s);
+        public void WriteLine(string s)
+        {
+            var dt = DateTime.UtcNow;
+            Buffer.Add(dt.Ticks, s);
 
-//            if (m_console || m_tw != null)
-//            {
-//                var ds = dt.ToLocalTime().ToString("yyyy/MM/dd HH:mm:ss ") + s;
-//                m_tw?.WriteLine(ds);
-//                if (m_console)
-//                    Console.WriteLine(ds);
-//            }
-//        }
+            if (m_console || m_tw != null)
+            {
+                var ds = dt.ToLocalTime().ToString("yyyy/MM/dd HH:mm:ss ") + s;
+                m_tw?.WriteLine(ds);
+                if (m_console)
+                    Console.WriteLine(ds);
+            }
+        }
 
-//        public void Flush() => m_tw?.Flush();
+        public void Flush() => m_tw?.Flush();
 
-//        public void Close()
-//        {
-//            if (m_tw != null)
-//            {
-//                m_tw.Flush();
-//                m_tw.Close();
-//                m_tw = null;
-//            }
-//        }
-//    }
+        public void Close()
+        {
+            if (m_tw != null)
+            {
+                m_tw.Flush();
+                m_tw.Close();
+                m_tw = null;
+            }
+        }
+    }
 }
