d41d1d11a5ec14cf25cd8d16b927466ea8f46a9b
Louis <trinewbee@qq.com>
2019-11-14 10:00:26 +0800

    修改Nano.Form二进制发布

--- nano/copybin.bat
+++ nano/copybin.bat
@@ -2,25 +2,37 @@
 set cpcmd=xcopy /d/y/q
 set tag=..\..\shared\cs
 
-echo Deploy components for PC
+rem Deploy components for PC
 for %%n in (
   Nano.Common Nano.Extensive
   Nano.Sockets Nano.Test
 ) do (
+  echo Project %%n
   for %%p in (net40 net45 net472 netcoreapp2.2 netcoreapp3.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
 )
 
-echo Deploy components for PC (Net 40 only)
-for %%n in (Nano.Forms Nano.Mysql Nano.Obsolete Nano.Rsa Nano.Win32 Nano.Xapi) do (
-    %cpcmd% %%n\bin\Release\%%n.dll %tag%\net40\
-    %cpcmd% %%n\bin\Release\%%n.pdb %tag%\net40\
+rem Deploy components for PC / Forms
+for %%n in (Nano.Forms) do (
+  echo Project %%n
+  for %%p in (net40 net45 net472 netcoreapp3.0) do (
+    %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
+    %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
+  )
 )
 
-echo Deploy components for Android
+rem Deploy components for PC (Net 40 only)
+for %%n in (Nano.Mysql Nano.Obsolete Nano.Rsa Nano.Win32 Nano.Xapi) do (
+  echo Project %%n
+  %cpcmd% %%n\bin\Release\%%n.dll %tag%\net40\
+  %cpcmd% %%n\bin\Release\%%n.pdb %tag%\net40\
+)
+
+rem Deploy components for Android
 for %%n in (Nano.Common Nano.Extensive Nano.Xapi Nano.Sockets) do (
+  echo Project %%n for Android
   %cpcmd% android\%%n\bin\Release\%%n.dll %tag%\android
   %cpcmd% android\%%n\bin\Release\%%n.pdb %tag%\android
 )
