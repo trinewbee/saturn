35c03edc0282826b7f95936a5c09b3fee37333b1
wenliangjun <wenliangjun@wenliangjundeMac-mini.local>
2019-11-25 18:00:54 +0800

    start redLock

---
+++ nano/Nano.Extensive/RedLock/RedLock.cs
@@ -0,0 +1,30 @@
+﻿using Nano.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nano.Extensive.RedLock
+{
+    public interface IRedLock : IDisposable 
+    {
+        
+    }
+
+    class RedLock : IRedLock
+    {
+        public TimeSpan expiredTime; // 默认10s
+        public TimeSpan waitTime; // 默认 
+        public TimeSpan retryTime; // 默认重试时间 1s
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRedLock CreateLock(string resource, )
+        {
+
+        }
+    }
+
+}
