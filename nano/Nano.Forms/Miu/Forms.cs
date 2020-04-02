0d1c2bcc5616d92dcab39a4a8d2702dc4d6dbfc8
trinewbee <trinewbee@gmail.com>
2020-04-02 15:12:08 +0800

    MiuViewManager

---
+++ nano/Nano.Forms/Miu/Forms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Nano.Forms.Miu
+{
+    public class MiuForm : Form
+    {
+        MiuViewManager m_manager;
+        Stack<MiuView> m_views;
+        MiuViewHost m_viewHost;
+        MiuView m_view = null;
+
+        public MiuForm(MiuViewManager manager)
+        {
+            m_manager = manager;
+            m_views = new Stack<MiuView>();
+            m_viewHost = new MiuViewHost(this, this);
+
+            StartPosition = FormStartPosition.CenterScreen;
+            Size = new Size(800, 450);
+            Load += Form_Load;
+            FormClosing += Form_Closing;
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            m_view = m_manager.Open();
+            m_views.Push(m_view);
+            m_view.InitUI(m_viewHost);
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            m_manager.Close();
+            m_manager = null;
+        }
+    }
+}
---
+++ nano/Nano.Forms/Miu/ViewHost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Nano.Forms.Miu
+{    
+    public class MiuViewHost
+    {
+        public Form Form { get; }
+        public Control Parent { get; }
+
+        public MiuViewHost(Form form, Control parent)
+        {
+            Form = form;
+            Parent = parent;
+        }
+    }
+
+    public interface MiuViewManager
+    {
+        MiuView Open();
+        void Close();
+    }
+
+    public interface MiuView : IDisposable
+    {
+        void InitUI(MiuViewHost host);
+    }
+}
