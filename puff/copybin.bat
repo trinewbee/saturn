1d6a5559681a5d31b678ae38fcdff77814546ea9
trinewbee <trinewbee@qq.com>
2021-06-22 10:54:30 +0800

    Update .net versions

--- puff/copybin.bat
+++ puff/copybin.bat
@@ -2,14 +2,14 @@ set cpcmd=xcopy /d/y/q
 set tag=..\..\shared\cs
 
 for %%n in (PuffServer) do (
-  for %%p in (net472 netcoreapp2.2 netcoreapp3.0) do (
+  for %%p in (net48 netcoreapp3.1 net5.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
 )
 
 for %%n in (PuffNetCore) do (
-  for %%p in (netcoreapp2.2 netcoreapp3.0) do (
+  for %%p in (netcoreapp3.1 net5.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
