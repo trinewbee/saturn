ded30b0bf908bbc8071f76afe7ae40235d5d0fbf
trinewbee <trinewbee@qq.com>
2021-06-22 09:25:45 +0800

    Update .net versions

--- nano/copybin.bat
+++ nano/copybin.bat
@@ -4,11 +4,11 @@ set tag=..\..\shared\cs
 
 rem Deploy components for PC
 for %%n in (
-  Nano.Common Nano.Extensive
+  Nano.Common Nano.Extensive Nano.Obsolete
   Nano.Sockets Nano.Test
 ) do (
   echo Project %%n
-  for %%p in (net40 net45 net472 netcoreapp2.2 netcoreapp3.0) do (
+  for %%p in (net48 netcoreapp3.1 net5.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
@@ -17,14 +17,21 @@ for %%n in (
 rem Deploy components for PC / Forms
 for %%n in (Nano.Forms) do (
   echo Project %%n
-  for %%p in (net40 net45 net472 netcoreapp3.0) do (
+  for %%p in (net48 netcoreapp3.1) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
 )
+for %%n in (Nano.Forms) do (
+  echo Project %%n-windows
+  for %%p in (net5.0) do (
+    %cpcmd% %%n\bin\Release\%%p-windows\%%n.dll %tag%\%%p\
+    %cpcmd% %%n\bin\Release\%%p-windows\%%n.pdb %tag%\%%p\
+  )
+)
 
 rem Deploy components for PC (Net 40 only)
-for %%n in (Nano.Mysql Nano.Obsolete Nano.Rsa Nano.Win32 Nano.Xapi) do (
+for %%n in (Nano.Mysql Nano.Rsa Nano.Win32 Nano.Xapi) do (
   echo Project %%n
   %cpcmd% %%n\bin\Release\%%n.dll %tag%\net40\
   %cpcmd% %%n\bin\Release\%%n.pdb %tag%\net40\
