72e7c316885c5a4b7f0818208dea78f58d502510
trinewbee <trinewbee@trinewbee.com>
2024-11-19 15:35:23 +0800

    net8.0

--- puff/copybin.bat
+++ puff/copybin.bat
@@ -2,14 +2,14 @@ set cpcmd=xcopy /d/y/q
 set tag=..\..\shared\cs
 
 for %%n in (PuffServer) do (
-  for %%p in (net48 netcoreapp3.1 net6.0) do (
+  for %%p in (net48 net6.0 net8.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
 )
 
 for %%n in (PuffNetCore) do (
-  for %%p in (netcoreapp3.1 net6.0) do (
+  for %%p in (net6.0 net8.0) do (
     %cpcmd% %%n\bin\Release\%%p\%%n.dll %tag%\%%p\
     %cpcmd% %%n\bin\Release\%%p\%%n.pdb %tag%\%%p\
   )
