6b23932f789580c22468069d0fcd6bf207b1686e
trinewbee <trinewbee@trinewbee.com>
2025-01-23 12:21:14 +0800

    TestSimpleWebSocket

---
+++ puff/demo/TestSimpleWebSocket/wstClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nano.Nuts;
+
+class WebSocketTestClientApp
+{
+	static async Task<string> Invoke(ClientWebSocket ws, string url, Dictionary<string, object> args)
+	{
+		args.Add("sc:m", url);
+		args.Add("sc:q", 3);
+		var o = DObject.New(args);
+		var str = DObject.ExportJsonStr(o);
+		await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(str)), WebSocketMessageType.Text, true, CancellationToken.None);
+
+		var buffer = new byte[0x100000];
+		var r = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+		Debug.Assert(r.EndOfMessage);
+		var str_t = Encoding.UTF8.GetString(buffer, 0, r.Count);
+		return str_t;
+	}
+
+	static async Task TestWebSockets()
+	{
+		var ws = new ClientWebSocket();
+		var uri = new Uri("ws://127.0.0.1:10002");
+		await ws.ConnectAsync(uri, CancellationToken.None);
+
+		var str = await Invoke(ws, "/api/ping", new());
+		Console.WriteLine(str);
+
+		str = await Invoke(ws, "/api/hello", new() { ["name"] = "Mandy", ["age"] = 12 });
+		Console.WriteLine(str);
+
+		await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "close", CancellationToken.None);
+	}
+
+	static void Main(string[] args)
+	{
+		var task = TestWebSockets();
+		task.Wait();
+	}
+}
---
+++ puff/demo/TestSimpleWebSocket/wstServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Nano.Logs;
+using Puff.Model;
+using Puff.Servers;
+
+[IceService(BaseUrl = "/api")]
+class TestApi
+{
+	[IceApi()]
+	public void Ping() { }
+
+	[IceApi(Ret = "m")]
+	public string Hello(string name, int age) => $"My name is {name}. I'm {age} yrs old.";
+}
+
+static class WebSocketTestServerApp
+{
+	static void StartServer()
+	{
+		uint portWeb = 10001, portWebSock = 10002;
+		var smb = new PuffServer();
+		try
+		{
+			smb.AddService(new TestApi());
+			smb.StartServer(portWeb: portWeb, portWebSock: portWebSock);
+			Console.WriteLine("Press enter key");
+			Console.ReadLine();
+			smb.StopServer();
+		}
+		catch (System.Net.HttpListenerException e)
+		{
+			Console.WriteLine("Failed to start HTTP server: " + e.Message);
+			Console.WriteLine("Try run the following command as administrator, then restart this application");
+			Console.WriteLine($"netsh http add urlacl url=http://+:{portWeb}/ user=Everyone");
+		}
+	}
+
+	static void Main(string[] args)
+	{
+		Logger.Init("logs", null, false, 69206016);
+		StartServer();
+	}
+}
